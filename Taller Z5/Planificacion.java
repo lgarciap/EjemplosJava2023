6ccc74ea0889b11dc067ca5122e90a3332fca9f4
lgarciap <lgarcia.uvg@gmail.com>
2023-09-07 08:43:53 -0600

    arreglo de empleados en Servicio

---
+++ Taller Z5/Planificacion.java
@@ -0,0 +1,9 @@
+public class Planificacion{
+	private Servicio[][] horario;
+	private Trabajador[] empleados;
+	
+	public Planificacion() {
+		horario = new Servicio[6][9];
+		empleados = new Trabajador[7];
+	}
+}
